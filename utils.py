f74253a05f03aefe40f4ad409180a3abfbf761b8
Zachary Ferguson <zfergus@nyu.edu>
2018-12-16 17:47:23 -0500

    Fixed OpenFace and detector

--- utils.py
+++ utils.py
@@ -20,7 +20,7 @@ def load_checkpoint(filename, model, optimizer, lr):
     for param_group in optimizer.param_groups:
         param_group['lr'] = lr
         print("Continuing training from checkpoint "
-              "{:s} at epoch {:d}\n".format(filename, start_epoch))
+              "{} at epoch {:d}\n".format(filename, start_epoch))
     return start_epoch
 
 
